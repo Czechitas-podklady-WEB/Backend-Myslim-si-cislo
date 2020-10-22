020675e66907924a5003aa305c2ec566646b6dbe
Filip Chalupa <chalupa.filip@gmail.com>
2020-10-23 00:50:57 +0200

    V1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express from 'express'
+import { cyrb53 } from './utils/cyrb53.js'
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -13,12 +14,74 @@ if (!port) {
 
 app.use(express.static('public'))
 
-const apiRouter = express.Router()
-app.use('/api', apiRouter)
+const createNumber = (min, max, id) => {
+	const hash = cyrb53(`${min}-${max}-${id}`)
+
+	return min + (hash % (max - min + 1))
+}
+
+const createPassword = (min, max, id) => {
+	const passwords = ['ryba', 'lachtan']
+	const index = createNumber(0, passwords.length - 1, `${min}-${max}-${id}`)
+	return passwords[index]
+}
+
+app.set('view engine', 'pug')
+
+// Home
+
+app.get('/', (request, response) => {
+	response.render('index')
+})
+
+// Game
+
+app.get('/nahodne-cislo/od/:from/do/:to/id/:id/', (request, response) => {
+	const { id } = request.params
+	const min = parseInt(request.params.from, 10) || 1
+	const max = Math.max(min + 1, parseInt(request.params.to, 10) || 1)
+	const guess = parseInt(request.query.tip, 10) || 0
+	const isGuessing = request.query.tip !== undefined
+
+	const limits = { min, max }
+
+	if (!isGuessing) {
+		response.render('rules', { min, max })
+		return
+	}
+
+	const target = createNumber(min, max, id)
+
+	if (guess === target) {
+		response.send({
+			message: `Trefa. 🎉 Opravdu jsem si myslel číslo ${target}. Za odměnu ti prozradím tajné heslo „${createPassword(
+				min,
+				max,
+				id,
+			)}“.`,
+			code: 'MATCH',
+		})
+		return
+	}
+
+	if (guess < target) {
+		response.send({
+			message: 'Myslím si větší číslo. 👆 Zkus to znovu.',
+			code: 'GUESS_TOO_LOW',
+			...limits,
+		})
+	} else {
+		response.send({
+			message: 'Myslím si menší číslo. 👇 Zkus to znovu.',
+			code: 'GUESS_TOO_HIGH',
+			...limits,
+		})
+	}
+})
 
 // 404
 
-apiRouter.use((request, response) => {
+app.use((request, response) => {
 	response.status(404).send({
 		status: 'error',
 		message: 'Not found.',
